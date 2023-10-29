4543769bfd1682af742e7dab50dc5873261577ad
Lakksh Sharma <lakkshsharma212@gmail.com>
2023-10-29 15:32:27 +0530

    Modified changes

--- lollipop.cpp
+++ lollipop.cpp
@@ -3,4 +3,6 @@ using namespace std;
 
 int main(){
 	cout<<"hello";
+	int a;
+	cout<<"indentations don't work but it seems to work just fine I guess. OMG it went antoher line. Damn. LEt's see if this works.";
 }
